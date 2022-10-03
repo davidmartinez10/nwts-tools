4b6d90bba1346bc6f7214b56f864e86017e658e6
David Martínez <david.mardav@gmail.com>
2022-10-03 09:40:06 -0300

    windows workaround

--- bin/package.mjs
+++ bin/package.mjs
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 import child_process from "node:child_process";
-import { promisify } from "node:util";
 import fs from "node:fs";
 import path from "node:path/posix";
 import win_path from "node:path/win32";
@@ -11,6 +10,9 @@ import { patch_nwjs_codecs } from "../patch-nwjs-codecs.mjs";
 const ignore = () => undefined;
 
 async function make_package() {
+  console.clear();
+  console.info("nwts-package\n");
+
   const temp_folder = await fs.promises.mkdtemp(path.join(os.tmpdir(), "build-"));
 
   const build_directory = (
@@ -25,22 +27,43 @@ async function make_package() {
   );
 
   let { displayName, dependencies, devDependencies } = JSON.parse(String(await fs.promises.readFile(path.join(process.cwd(), "package.json"))));
-  const preserve_spaces = `🇺🇦${Number.MAX_SAFE_INTEGER}🇺🇦`;
-  displayName.replace(" ", preserve_spaces);
 
   const { nw: nw_version } = devDependencies;
   const version = process.env.NWJS_VERSION || nw_version.replace("-sdk", "");
 
+  const config = {
+    "Application name": displayName,
+    "Current working directory": process.cwd(),
+    "Build directory": build_directory,
+    "Package directory": package_directory,
+    "NW.js version": version,
+    "Package type": process.env.PACKAGE_TYPE || "normal",
+  };
+
+  console.info("Running on these settings:");
+  console.table(config);
+
+  const preserve_spaces = `🇺🇦${Number.MAX_SAFE_INTEGER}🇺🇦`;
+  displayName.replace(" ", preserve_spaces);
+
   // Fetch runtime dependencies
   await fs.promises.writeFile(path.join(temp_folder, "package.json"), JSON.stringify({ dependencies }), { encoding: "utf8" });
-  await promisify(child_process.exec)(`npm install`, { "cwd": temp_folder });
+  child_process.execSync(`npm install`, { "cwd": temp_folder, stdio: "inherit" });
 
   const has_runtime_deps = Object.values(dependencies).length > 0;
+
+  if (has_runtime_deps) {
+    console.info("\nBundling these runtime dependencies:");
+    console.table(dependencies);
+  }
+
   const runtime_modules = path.join(temp_folder, "runtime_modules");
 
-  if (has_runtime_deps) await fs.promises.rename(path.join(temp_folder, "node_modules"), runtime_modules);
+  if (has_runtime_deps) {
+    await fs.promises.rename(path.join(temp_folder, "node_modules"), runtime_modules);
+  }
 
-  await promisify(child_process.exec)(`npm install nw@${version}`, { "cwd": temp_folder });
+  child_process.execSync(`npm install nw@${version}`, { "cwd": temp_folder, stdio: "inherit" });
 
   if (process.env.NWJS_FFMPEG === "PATCH") {
     const { findpath } = await import(
@@ -64,13 +87,79 @@ async function make_package() {
       const app_directory = win_path.join(".", package_directory, displayName);
 
       commands.push(`Robocopy "${win_path.join(temp_folder, "node_modules/nw/nwjs")}" "${app_directory}" *.* /E /MOVE`);
-      if (has_runtime_deps) commands.push(`Robocopy "${runtime_modules}" "${app_directory}\\node_modules" *.* /E /MOVE`);
 
+      if (has_runtime_deps) {
+        child_process.spawnSync("Robocopy", [
+          JSON.stringify(path.normalize(runtime_modules)),
+          JSON.stringify(path.join(build_directory, "node_modules")),
+          "*.*", "/E", "/MOVE"
+        ],
+          {
+            shell: true,
+            cwd: process.cwd(),
+            env: process.env,
+            stdio: "inherit",
+            encoding: "utf-8",
+          }
+        );
+      }
 
       if (process.env.PACKAGE_TYPE === "zip") {
         const nw = win_path.join(app_directory, "nw.exe");
         const package_nw = win_path.join(app_directory, "package.nw");
         const package_zip = win_path.join(temp_folder, "package.zip");
+
+        let with_dirs = false;
+        for await (const item of await fs.promises.opendir(build_directory)) {
+          if (item.isDirectory()) {
+            const dirpath = win_path.join(build_directory, item.name);
+            commands.push(`powershell Compress-Archive "${dirpath}" "${dirpath}.zip"`);
+            commands.push(`powershell Remove-Item "${dirpath}" -Recurse`);
+            with_dirs = true;
+          }
+        }
+
+        if (with_dirs) {
+          const manifest = JSON.parse(String(await fs.promises.readFile(path.join(build_directory, "package.json"))));
+
+          const manifest_main = String(await fs.promises.readFile(path.join(build_directory, manifest.main)));
+          let launcher_js;
+
+          if (path.extname(manifest.main) === ".html") {
+            const scripts = manifest_main.split("\n")
+              .filter(function predicate(value) {
+                return value.includes("<script");
+              })
+              .map(function callbackfn(value) {
+                const script_src = value.split(`src="`)[1].split(`"`)[0];
+                return script_src;
+              })
+              .filter(function predicate(value) {
+                return [".js", ".cjs", ".mjs"].includes(path.extname(value)) && value[0] === "/";
+              });
+
+            launcher_js = String(await fs.promises.readFile(path.join(build_directory, scripts[0])));
+          } else {
+            launcher_js = manifest_main;
+          }
+
+          async function expander() {
+            const win = nw.Window.get();
+            win.hide();
+            for await (const item of await require("node:fs/promises").opendir(".")) {
+              if (require("node:path").extname(item.name) === ".zip") {
+                const { execSync } = require("node:child_process");
+                execSync(`powershell Expand-Archive ${item.name} -DestinationPath .`);
+                execSync(`powershell Remove-Item ${item.name}`);
+              }
+            }
+            win.show();
+            "__launcher__";
+          }
+
+          const script = `${String(expander).replace(`"__launcher__";`, launcher_js)}; expander();`;
+          await fs.promises.writeFile(path.join(build_directory, "launcher.js"), script);
+        }
 
         commands.push(`cd "${build_directory}" && powershell Compress-Archive ".\\*" "${package_zip}"`);
         commands.push(`move "${package_zip}" "${package_nw}"`);
@@ -125,7 +214,7 @@ async function make_package() {
       shell: true,
       cwd: process.cwd(),
       env: process.env,
-      stdio: [process.stdin, process.stdout, process.stderr],
+      stdio: "inherit",
       encoding: "utf-8",
     });
   }
