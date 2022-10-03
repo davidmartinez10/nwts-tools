7d6a5d4aa46f6da20d561250ef988bd3a2bed7d2
David Martínez <david.mardav@gmail.com>
2022-10-03 09:39:48 -0300

    minor improvements

--- patch-nwjs-codecs.mjs
+++ patch-nwjs-codecs.mjs
@@ -22,16 +22,15 @@ export async function patch_nwjs_codecs(nw_path) {
   const nw = dependencies?.nw || devDependencies?.nw;
   const version = nw.replace("-sdk", "");
 
-  const { stdout: url } = await promisify(child_process.exec)(
+  const url = String(child_process.execSync(
     `npx -y nwjs-ffmpeg-prebuilt -v ${version} --get-download-url`
-  );
+  ));
 
   const temp_folder = await fs.promises.mkdtemp(
     path.join(os.tmpdir(), "nwjs-ffmpeg-")
   );
 
   const ffmpeg_zip = path.join(temp_folder, "ffmpeg.zip");
-
   await promisify(stream.pipeline)(got.stream(url), fs.createWriteStream(ffmpeg_zip));
 
   switch (os.platform()) {
@@ -46,10 +45,11 @@ export async function patch_nwjs_codecs(nw_path) {
       break;
   }
 
-  await promisify(child_process.exec)(
+  child_process.execSync(
     os.platform() === "win32"
-      ? `powershell Expand-Archive ${ffmpeg_zip} -DestinationPath ${nw_path}`
-      : `unzip -o ${JSON.stringify(ffmpeg_zip)} -d ${JSON.stringify(nw_path)}`
+      ? `powershell Expand-Archive ${ffmpeg_zip} -DestinationPath ${nw_path} -Force`
+      : `unzip -o ${JSON.stringify(ffmpeg_zip)} -d ${JSON.stringify(nw_path)}`,
+    { stdio: "inherit" }
   );
 
   await fs.promises.unlink(temp_folder).catch(ignore);
