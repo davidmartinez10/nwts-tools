51395e47de19681620547caf1464ca79e2a6df97
David Martínez <david.mardav@gmail.com>
2022-10-03 15:39:50 -0300

    more explicit envvar definitions

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,17 +1,23 @@
+type IntString = `${number}`;
+
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NODE_ENV: 'development' | 'production';
-      EXECUTION_MODE?: "DEBUG";
-      LAUNCHER_PORT?: string;
-      DEBUG_PORT?: string;
-      // nwts-package
+      NODE_ENV: 'development' | 'production' | undefined;
+      EXECUTION_MODE?: "DEBUG" | undefined;
+      LAUNCHER_PORT?: IntString;
+      DEBUG_PORT?: IntString | undefined;
+
+      // *nwts-package*
+      // Refer to: https://nwjs.readthedocs.io/en/latest/For%20Users/Package%20and%20Distribute/
       BUILD_DIRECTORY?: string; // default: "build"
       PACKAGE_DIRECTORY?: string; // default: "dist"
+      PACKAGE_TYPE?: "plain" | "zip" | "zip+exe"; // default: "plain"; "zip+exe" isn't available on Mac and it falls back to "zip"
+      NWJS_VERSION?: string; // defaults to the version set in your package.json/devDependencies.nw, minus '-sdk'
+
+      // Check your 'nw' version matches one of the prebuilt binaries here:
+      // https://github.com/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases
       NWJS_FFMPEG?: "PATCH";
-      PACKAGE_TYPE?: "normal" | "zip";
-      // & nwts-patch-codecs
-      NWJS_VERSION?: string;
     }
   }
 }
