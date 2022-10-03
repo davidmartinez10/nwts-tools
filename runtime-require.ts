e6314a43411069d559ede327857cbdb479add008
David Martínez <david.mardav@gmail.com>
2022-10-03 00:39:11 -0300

    runtime-require ts and cjs modules

--- runtime-require.ts
+++ runtime-require.ts
@@ -1,16 +1,13 @@
-import os from "node:os";
-import path from "node:path";
+import os from "./node/os";
+import path from "./node/path";
 
 const is_win = os.platform() === "win32";
 const cwd = path.dirname(process.execPath);
 
-/**
- * @param {string} id
- */
-export function runtime_require(id) {
+export function runtime_require<T>(id: string) {
   return require(
     is_win && !cwd.includes("node_modules")
       ? path.join(cwd, "node_modules", id)
       : id
-  );
+  ) as T;
 }
