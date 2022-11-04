86f82cc2bf3e156f67022fba4b8e34fa4def4341
David Martínez <david.mardav@gmail.com>
2022-11-04 15:17:42 -0300

    fix regression

--- bin/patch-codecs.mjs
+++ bin/patch-codecs.mjs
@@ -1,41 +1,9 @@
-#!/usr/bin/env zx
-import {promisify} from "node:util";
-import stream from "node:stream";
-import "zx/globals";
-import {get_nwjs_path} from "nwts-tools/nwjs-path.mjs";
-import got from "got";
+#!/usr/bin/env node
+import { get_nwjs_path } from "nwts-tools/nwjs-path.mjs";
+import { patch_nwjs_codecs } from "nwts-tools/patch-nwjs-codecs.mjs";
 
-const ignore = () => void 0;
-
-const { dependencies, devDependencies }
-= await fs.readJSON(path.join(process.cwd(), "package.json"));
-
-const nw      = dependencies?.nw || devDependencies?.nw;
-const version = nw.replace("-sdk", "");
-
-const { stdout: url }
-= await $`npx -y nwjs-ffmpeg-prebuilt -v ${version} --get-download-url`;
-
-const temp_folder = await fs.mkdtemp(path.join(os.tmpdir(), "nwjs-ffmpeg-"));
-
-const ffmpeg_zip = path.join(temp_folder, "ffmpeg.zip");
-await promisify(stream.pipeline)(got.stream(url),
-                                 fs.createWriteStream(ffmpeg_zip));
-
-let nw_path = await get_nwjs_path();
-
-switch (os.platform()) {
-case "darwin":
-  nw_path
-    = nw_path.replace("MacOS/nwjs",
-                      "Frameworks/nwjs Framework.framework/Versions/Current");
-  break;
-case "win32": nw_path = nw_path.replace("nw.exe", ""); break;
-case "linux": nw_path = nw_path.replace("nw/nwjs/nw", "nw/nwjs/lib"); break;
+async function patch_codecs() {
+  patch_nwjs_codecs(await get_nwjs_path());
 }
 
-await $`${os.platform() === "win32"
-  ? `powershell Expand-Archive ${ffmpeg_zip} -DestinationPath ${nw_path} -Force`
-  : `unzip -o ${JSON.stringify(ffmpeg_zip)} -d ${JSON.stringify(nw_path)}`}`;
-
-await fs.unlink(temp_folder).catch(ignore);
+patch_codecs();
